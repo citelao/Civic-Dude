43ba781be6c642bdba8e5e7efd16b9126b97b0f3
Ben Stolovitz <ben@stolovitz.com>
2025-09-25 23:12:52 -0400

    playin

--- contrib/scrapy/README.md
+++ contrib/scrapy/README.md
@@ -4,4 +4,6 @@
 # uv tool run scrapy ...
 uv tool run scrapy crawl meetings
 uv tool run scrapy shell "https://bellevue.legistar.com/Feed.ashx?M=Calendar&ID=31515925&GUID=fe06a265-908f-4b2e-b0bf-5570c51f012c&Mode=All"
+
+uv tool run scrapy parse "https://bellevue.legistar.com/MeetingDetail.aspx?From=RSS&ID=1340044&GUID=08396C01-1563-4E18-B521-B489520E087A" --spider meetings -c parse_meeting
 ```
--- contrib/scrapy/legistar/settings.py
+++ contrib/scrapy/legistar/settings.py
@@ -46,9 +46,9 @@ DOWNLOAD_DELAY = 1
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "legistar.middlewares.LegistarDownloaderMiddleware": 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+   "legistar.middlewares.LegistarDownloaderMiddleware": 543,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
